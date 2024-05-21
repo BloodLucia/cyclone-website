1311e11b98e454d316b9f1c8597ddb5d69994a6e
Dennis <thecvcoder@foxmail.com>
2024-05-21 19:15:20 +0800

    修改站点名称

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,11 @@
-export const SITE_TITLE = 'Dreamy Coastline'
+export const SITE_TITLE = 'Cyclone赛科隆'
+export const SITE_EN_NAME = 'Cyclone'
 export const SITE_DESC = `${SITE_TITLE} is rock band from China.`
 export const SITE_URL = "/"
 
 export const MEDIA_LINKS = [
-  { icon: "wyy", link: "/" },
-  { icon: "qqyy", link: "/" },
+  { icon: "wyy", link: "https://music.163.com/#/artist?id=55015012&userid=559942376" },
+  { icon: "qqyy", link: "https://y.qq.com/n/ryqq/singer/000CnITT3mynZ2" },
   { icon: "spotify", link: "/" },
   { icon: "twitter", link: "/" },
   { icon: "instagram", link: "/" },
