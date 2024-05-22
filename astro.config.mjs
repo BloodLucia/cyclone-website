6d1aafa2c5153ca2525ba37f0894dee635b0a4f3
Dennis <thecvcoder@foxmail.com>
2024-05-22 11:12:35 +0800

    导入passthroughImageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import zeabur from '@zeabur/astro-adapter/serverless';
